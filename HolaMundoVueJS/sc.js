1d7e3129e5869b0a814918aa2ae755544ab6e512
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-05-22 15:17:02 -0700

    Hola mundo en Vue.js

---
+++ HolaMundoVueJS/sc.js
@@ -0,0 +1,68 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Steven es un men manco xD',
+    message2: 'este es el objeto data'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+  	message: 'Este es un demo de VUeJS',
+    tooltip: 'Este es un tooltip'
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
