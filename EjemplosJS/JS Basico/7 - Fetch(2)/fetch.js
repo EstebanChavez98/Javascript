7d29a99fc0f529b9142fbb257146b47d07041540
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-05-27 14:15:25 -0700

    Mas ejemplos de Fetch

--- EjemplosJS/JS Basico/7 - Fetch(2)/fetch.js
+++ EjemplosJS/JS Basico/7 - Fetch(2)/fetch.js
@@ -4,9 +4,7 @@ var contenido = document.querySelector('#contenido');
             .then(res => res.json())
             .then(data => {
                 console.log(data.results['0'].email);
-                contenido.innerHTML = `
-                <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle">
-            <p>Nombre: ${data.results['0'].name.first}</p>
-                `
+                contenido.innerHTML = `<img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle">
+            <p>Nombre: ${data.results['0'].name.first}</p>`
             })
         }
